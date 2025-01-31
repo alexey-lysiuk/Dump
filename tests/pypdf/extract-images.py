8a7152f83a3ed5bc81dbf759fa6aae9695c82f57
alexey.lysiuk <alexey.lysiuk@gmail.com>
2025-01-31 15:25:45 +0200

    extract pdf images

---
+++ tests/pypdf/extract-images.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import sys
+import pypdf
+
+def _extract_images(pdfpath: str):
+    reader = pypdf.PdfReader(pdfpath)
+
+    for pageindex, page in enumerate(reader.pages):
+        for imageindex, image in enumerate(page.images):
+            imagepath = f'{image.name}_{pageindex}_{imageindex}'
+
+            with open(imagepath, "wb") as f:
+                f.write(image.data)
+
+def _main():
+    for path in sys.argv[:1]:
+        _extract_images(path)
+
+if __name__ == '__main__':
+    _main()
