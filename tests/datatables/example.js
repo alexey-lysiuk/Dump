5f59e65ecb3ac1620f2ff33febe995ba9d3d0f42
alexey.lysiuk <alexey.lysiuk@gmail.com>
2025-02-20 17:18:47 +0200

    more work on datadatles.js example

--- tests/datatables/example.js
+++ tests/datatables/example.js
@@ -1,50 +1,56 @@
-var dataSet = [
-    ['Tiger Nixon', 'System Architect', 'Edinburgh', '5421', '2011/04/25', '$320,800'],
-    ['Garrett Winters', 'Accountant', 'Tokyo', '8422', '2011/07/25', '$170,750'],
-    ['Ashton Cox', 'Junior Technical Author', 'San Francisco', '1562', '2009/01/12', '$86,000'],
-    ['Cedric Kelly', 'Senior Javascript Developer', 'Edinburgh', '6224', '2012/03/29', '$433,060'],
-    ['Airi Satou', 'Accountant', 'Tokyo', '5407', '2008/11/28', '$162,700'],
-    ['Brielle Williamson', 'Integration Specialist', 'New York', '4804', '2012/12/02', '$372,000'],
-    ['Herrod Chandler', 'Sales Assistant', 'San Francisco', '9608', '2012/08/06', '$137,500'],
-    ['Rhona Davidson', 'Integration Specialist', 'Tokyo', '6200', '2010/10/14', '$327,900'],
-    ['Colleen Hurst', 'Javascript Developer', 'San Francisco', '2360', '2009/09/15', '$205,500'],
-    ['Sonya Frost', 'Software Engineer', 'Edinburgh', '1667', '2008/12/13', '$103,600'],
-    ['Jena Gaines', 'Office Manager', 'London', '3814', '2008/12/19', '$90,560'],
-    ['Quinn Flynn', 'Support Lead', 'Edinburgh', '9497', '2013/03/03', '$342,000'],
-    ['Charde Marshall', 'Regional Director', 'San Francisco', '6741', '2008/10/16', '$470,600'],
-    ['Haley Kennedy', 'Senior Marketing Designer', 'London', '3597', '2012/12/18', '$313,500'],
-    ['Tatyana Fitzpatrick', 'Regional Director', 'London', '1965', '2010/03/17', '$385,750'],
-    ['Michael Silva', 'Marketing Designer', 'London', '1581', '2012/11/27', '$198,500'],
-    ['Paul Byrd', 'Chief Financial Officer (CFO)', 'New York', '3059', '2010/06/09', '$725,000'],
-    ['Gloria Little', 'Systems Administrator', 'New York', '1721', '2009/04/10', '$237,500'],
-    ['Bradley Greer', 'Software Engineer', 'London', '2558', '2012/10/13', '$132,000'],
-    ['Dai Rios', 'Personnel Lead', 'Edinburgh', '2290', '2012/09/26', '$217,500'],
-    ['Jenette Caldwell', 'Development Lead', 'New York', '1937', '2011/09/03', '$345,000'],
-    ['Yuri Berry', 'Chief Marketing Officer (CMO)', 'New York', '6154', '2009/06/25', '$675,000'],
-    ['Caesar Vance', 'Pre-Sales Support', 'New York', '8330', '2011/12/12', '$106,450'],
-    ['Doris Wilder', 'Sales Assistant', 'Sydney', '3023', '2010/09/20', '$85,600'],
-    ['Angelica Ramos', 'Chief Executive Officer (CEO)', 'London', '5797', '2009/10/09', '$1,200,000'],
-    ['Gavin Joyce', 'Developer', 'Edinburgh', '8822', '2010/12/22', '$92,575'],
-    ['Jennifer Chang', 'Regional Director', 'Singapore', '9239', '2010/11/14', '$357,650'],
-    ['Brenden Wagner', 'Software Engineer', 'San Francisco', '1314', '2011/06/07', '$206,850'],
-    ['Fiona Green', 'Chief Operating Officer (COO)', 'San Francisco', '2947', '2010/03/11', '$850,000'],
-    ['Shou Itou', 'Regional Marketing', 'Tokyo', '8899', '2011/08/14', '$163,000'],
-    ['Michelle House', 'Integration Specialist', 'Sydney', '2769', '2011/06/02', '$95,400'],
-    ['Suki Burks', 'Developer', 'London', '6832', '2009/10/22', '$114,500'],
-    ['Prescott Bartlett', 'Technical Author', 'London', '3606', '2011/05/07', '$145,000'],
-    ['Gavin Cortez', 'Team Leader', 'San Francisco', '2860', '2008/10/26', '$235,500'],
-    ['Martena Mccray', 'Post-Sales support', 'Edinburgh', '8240', '2011/03/09', '$324,050'],
-    ['Unity Butler', 'Marketing Designer', 'San Francisco', '5384', '2009/12/09', '$85,675'],
+var dataSet =	[
+	['Product 1', [[10, 20]]],
+	['Product 2', [[10, 20], [30, 40], [50, 60]]],
+	['Product 3', [[100, 200]]],
+	['Product 4', [[10, 20], [30, 40], [300, 400]]],
+	['Product 5', [[2000, 2500], [3000, 3500]]],
+	['Product 6', [[1000, 1200], [2000, 2200]]],
+	['Product 7', [[10, 20], [100, 200]]],
+	['Product 8', [[10, 20], [30, 40], [100, 200]]],
+	['Product 9', [[10, 90], [300, 400], [1000, 1200], [2000, 2200]]],
+	['Product 10', [[110, 120], [130, 140], [150, 160]]],
+	['Product 11', [[10, 40], [90, 110]]],
+	['Product 12', [[100, 200], [300, 400], [500, 600]]],
+	['Product 13', [[1000, 1200]]],
+	['Product 14', [[100, 200], [500, 600]]],
+	['Product 15', [[300, 400]]],
+	['Product 16', [[100, 200], [300, 400]]],
+	['Product 17', [[10, 60]]],
+	['Product 18', [[1000, 1200], [2000, 2200], [3000, 3200]]],
+	['Product 19', [[200, 300], [530, 540]]],
+	['Product 20', [[100, 500]]]
 ];
- 
-new DataTable('#example', {
+
+
+let table = new DataTable('#example', {
     columns: [
         { title: 'Name' },
-        { title: 'Position' },
-        { title: 'Office' },
-        { title: 'Extn.' },
-        { title: 'Start date' },
-        { title: 'Salary' }
+        { title: 'Range' }
     ],
     data: dataSet
+});
+
+function SearchRanges(string, row, rowindex)
+{
+	let value = myInput.value
+
+	if (value == '')
+		return true;
+
+	value = Number(value)
+
+	if (Number.isNaN(value))
+		return true;
+
+	for (let range of row[1])
+	{
+		if (value >= range[0] && value <= range[1])
+			return true;
+	}
+
+	return false;
+}
+
+$('#myInput').on('keyup', function () {
+	table.search(SearchRanges).draw();
 });
